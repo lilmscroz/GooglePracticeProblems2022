ac59b350c2e1f5030748981c0b65822daefd7760
rmcrosley <rmcrosley@alaska.edu>
2022-07-18 21:28:52 -0800

    add initial solution and notes

---
+++ GooglePracticeProblems/GooglePracticeProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using Problem1Namespace;
+
+namespace GooglePracticeProblems
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
